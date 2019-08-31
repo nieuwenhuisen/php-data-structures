40174a61340b733778d56ebfec484c246bae51ea
Jeroen van den Nieuwenhuisen <nieuwenhuisen@me.com>
2019-08-31 21:29:49 +0200

    More doubly linked list functions

--- src/LinkedList/DoublyLinkedList.php
+++ src/LinkedList/DoublyLinkedList.php
@@ -9,8 +9,12 @@ class DoublyLinkedList implements Countable
     public const DIRECTION_FORWARD = 'DIRECTION_FORWARD';
     public const DIRECTION_BACKWARD = 'DIRECTION_BACKWARD';
 
+    /** @var ListNode|null */
     private $firstNode;
+
+    /** @var ListNode|null */
     private $lastNode;
+
     private $totalNodes = 0;
 
     private function createNode($data)
@@ -45,6 +49,119 @@ class DoublyLinkedList implements Countable
             $currentLastNode->next = $newNode;
             $newNode->previous = $currentLastNode;
             $this->lastNode = $newNode;
+        }
+    }
+
+    public function insertBefore($data, $query): void
+    {
+        /** @var ListNode $newNode */
+        if ($newNode = $this->createNode($data)) {
+            $previous = null;
+            $currentNode = $this->firstNode;
+
+            while ($currentNode) {
+                if ($currentNode->data === $query) {
+                    $currentNode->previous = $newNode;
+                    $previous->next = $newNode;
+                    $newNode->next = $currentNode;
+                    $newNode->previous = $previous;
+                    break;
+                }
+
+                $previous = $currentNode;
+                $currentNode = $currentNode->next;
+            }
+        }
+    }
+
+    public function insertAfter($data, $query): void
+    {
+        /** @var ListNode $newNode */
+        if ($newNode = $this->createNode($data)) {
+            $nextNode = null;
+            $currentNode = $this->firstNode;
+
+            while ($currentNode) {
+                if ($currentNode->data === $query) {
+                    if ($nextNode) {
+                        $newNode->next = $nextNode;
+                    }
+
+                    if ($currentNode === $this->lastNode) {
+                        $this->lastNode = $newNode;
+                    }
+
+                    $currentNode->next = $newNode;
+                    $nextNode->previous = $newNode;
+                    $newNode->previous = $currentNode;
+                    break;
+                }
+                $currentNode = $currentNode->next;
+                $nextNode = $currentNode->next;
+            }
+        }
+    }
+
+    public function deleteFirst(): void
+    {
+        if ($this->count() === 0) {
+            return;
+        }
+
+        if ($this->firstNode->next) {
+            $this->firstNode = $this->firstNode->next;
+            $this->firstNode->previous = null;
+        } else {
+            $this->firstNode = null;
+        }
+
+        $this->totalNodes--;
+    }
+
+    public function deleteLast(): void
+    {
+        if ($this->count() === 0) {
+            return;
+        }
+
+        $currentNode = $this->lastNode;
+
+        if (!$currentNode->previous) {
+            $this->firstNode = null;
+            $this->lastNode = null;
+        } else {
+            $previousNode = $currentNode->previous;
+            $this->lastNode = $previousNode;
+            $previousNode->next = null;
+        }
+
+        $this->totalNodes--;
+    }
+
+    public function delete($query): void
+    {
+        if ($this->count() === 0) {
+            return;
+        }
+
+        $previous = null;
+        $currentNode = $this->firstNode;
+
+        while ($currentNode) {
+            if ($currentNode->data === $query) {
+                if (!$currentNode->next) {
+                    $previous->next = null;
+                } else {
+                    $previous->next = $currentNode->next;
+                    $currentNode->next->previous = $previous;
+                }
+
+                $this->totalNodes--;
+                break;
+            }
+
+            $previous = $currentNode;
+            $currentNode = $currentNode->next;
         }
     }
 
--- tests/DoublyLinkedListTest.php
+++ tests/DoublyLinkedListTest.php
@@ -19,11 +19,21 @@ class DoublyLinkedListTest extends TestCase
         $this->linkedList->insertAtFirst('Item 1');
         $this->linkedList->insertAtFirst('Item 2');
 
+        $this->assertEquals(
+            [
+                'Item 2',
+                'Item 1',
+            ],
+            $this->linkedList->toArray()
+        );
+    }
+
+    public function testCount(): void
+    {
+        $this->linkedList->insertAtFirst('Item 1');
+        $this->linkedList->insertAtFirst('Item 2');
+
         $this->assertCount(2, $this->linkedList);
-        $this->assertEquals([
-            'Item 2',
-            'Item 1',
-        ], $this->linkedList->toArray());
     }
 
     public function testInsertAtLast(): void
@@ -31,11 +41,112 @@ class DoublyLinkedListTest extends TestCase
         $this->linkedList->insertAtLast('Item 1');
         $this->linkedList->insertAtLast('Item 2');
 
+        $this->assertEquals(
+            [
+                'Item 1',
+                'Item 2',
+            ],
+            $this->linkedList->toArray()
+        );
+    }
+
+    public function testInsertBefore(): void
+    {
+        $this->linkedList->insertAtLast('Item 1');
+        $this->linkedList->insertAtLast('Item 2');
+        $this->linkedList->insertAtLast('Item 4');
+        $this->linkedList->insertBefore('Item 3', 'Item 4');
+
+        $this->assertEquals(
+            [
+                'Item 1',
+                'Item 2',
+                'Item 3',
+                'Item 4',
+            ],
+            $this->linkedList->toArray()
+        );
+    }
+
+    public function testInsertAfter(): void
+    {
+        $this->linkedList->insertAtLast('Item 1');
+        $this->linkedList->insertAtLast('Item 2');
+        $this->linkedList->insertAtLast('Item 4');
+        $this->linkedList->insertAfter('Item 3', 'Item 2');
+
+        $this->assertEquals(
+            [
+                'Item 1',
+                'Item 2',
+                'Item 3',
+                'Item 4',
+            ],
+            $this->linkedList->toArray()
+        );
+    }
+
+    public function testDeleteFirst(): void
+    {
+        $this->linkedList->insertAtLast('Item 1');
+        $this->linkedList->insertAtLast('Item 2');
+        $this->linkedList->insertAtLast('Item 3');
+        $this->linkedList->insertAtLast('Item 4');
+
+        $this->linkedList->deleteFirst();
+
+        $this->assertCount(3, $this->linkedList);
+
+        $this->assertEquals(
+            [
+                'Item 2',
+                'Item 3',
+                'Item 4',
+            ],
+            $this->linkedList->toArray()
+        );
+    }
+
+    public function testDeleteLast(): void
+    {
+        $this->linkedList->insertAtLast('Item 1');
+        $this->linkedList->insertAtLast('Item 2');
+        $this->linkedList->insertAtLast('Item 3');
+        $this->linkedList->insertAtLast('Item 4');
+
+        $this->linkedList->deleteLast();
+
+        $this->assertCount(3, $this->linkedList);
+
+        $this->assertEquals(
+            [
+                'Item 1',
+                'Item 2',
+                'Item 3',
+            ],
+            $this->linkedList->toArray()
+        );
+    }
+
+    public function testDelete(): void
+    {
+        $this->linkedList->insertAtLast('Item 1');
+        $this->linkedList->insertAtLast('Item 2');
+        $this->linkedList->insertAtLast('Item 3');
+        $this->linkedList->insertAtLast('Item 4');
+
+        $this->linkedList->delete('Item 2');
+        $this->linkedList->delete('Item 3');
+
         $this->assertCount(2, $this->linkedList);
-        $this->assertEquals([
-            'Item 1',
-            'Item 2',
-        ], $this->linkedList->toArray());
+
+        $this->assertEquals(
+            [
+                'Item 1',
+                'Item 4',
+            ],
+            $this->linkedList->toArray()
+        );
     }
 
     public function testToArray(): void
@@ -44,16 +155,26 @@ class DoublyLinkedListTest extends TestCase
         $this->linkedList->insertAtLast('Item 2');
         $this->linkedList->insertAtLast('Item 3');
 
-        $this->assertEquals([
-            'Item 1',
-            'Item 2',
-            'Item 3',
-        ], $this->linkedList->toArray(DoublyLinkedList::DIRECTION_FORWARD));
+        $this->assertEquals(
+            [
+                'Item 1',
+                'Item 2',
+                'Item 3',
+            ],
+            $this->linkedList->toArray(
+                DoublyLinkedList::DIRECTION_FORWARD
+            )
+        );
 
-        $this->assertEquals([
-            'Item 3',
-            'Item 2',
-            'Item 1',
-        ], $this->linkedList->toArray(DoublyLinkedList::DIRECTION_BACKWARD));
+        $this->assertEquals(
+            [
+                'Item 3',
+                'Item 2',
+                'Item 1',
+            ],
+            $this->linkedList->toArray(
+                DoublyLinkedList::DIRECTION_BACKWARD
+            )
+        );
     }
 }
