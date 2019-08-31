b3fb5497c48ad2549db2375081a4484f2b61829f
Jeroen van den Nieuwenhuisen <nieuwenhuisen@me.com>
2019-08-31 22:11:18 +0200

    Add linked list queue data structure

---
+++ src/Queue/LinkedListQueue.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Queue;
+
+use App\LinkedList\LinkedList;
+use Countable;
+use OverflowException;
+use UnderflowException;
+
+class LinkedListQueue implements QueueInterface, Countable
+{
+    private $limit;
+    private $queue;
+
+    /**
+     * @param int $limit The limit of the queue, use 0 for unlimited
+     */
+    public function __construct(int $limit = 0)
+    {
+        $this->limit = $limit;
+        $this->queue = new LinkedList();
+    }
+
+    public function enqueue($item): void
+    {
+        if ($this->limit > 0 && count($this->queue) >= $this->limit) {
+            throw new OverflowException('Queue limit reached');
+        }
+
+        $this->queue->insert($item);
+    }
+
+    public function dequeue()
+    {
+        if ($this->isEmpty()) {
+            throw new UnderflowException('Queue is empty');
+        }
+
+        $lastItem = $this->peek();
+        $this->queue->deleteFirst();
+        return $lastItem;
+    }
+
+    public function peek()
+    {
+        return $this->queue->getLinkNode(0)->data;
+    }
+
+    public function count(): int
+    {
+        return $this->queue->count();
+    }
+
+    public function isEmpty(): bool
+    {
+        return 0 === $this->count();
+    }
+}
---
+++ tests/LinkedListQueueTest.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Tests;
+
+use App\Queue\LinkedListQueue;
+use OverflowException;
+use PHPUnit\Framework\TestCase;
+use UnderflowException;
+
+class LinkedListQueueTest extends TestCase
+{
+    private $queue;
+
+    public function setUp(): void
+    {
+        $this->queue = new LinkedListQueue(5);
+    }
+
+    public function testQueueIsEmpty(): void
+    {
+        $this->assertTrue($this->queue->isEmpty());
+    }
+
+    public function testQueuePush(): void
+    {
+        $this->queue->enqueue('Item 1');
+        $this->queue->enqueue('Item 2');
+        $this->queue->enqueue('Item 3');
+
+        $this->assertCount(3, $this->queue);
+    }
+
+    public function testQueuePop(): void
+    {
+        $this->queue->enqueue('Item 1');
+        $this->queue->enqueue('Item 2');
+        $this->queue->enqueue('Item 3');
+
+        $item1 = $this->queue->dequeue();
+        $item2 = $this->queue->dequeue();
+
+        $this->assertCount(1, $this->queue);
+        $this->assertSame('Item 1', $item1);
+        $this->assertSame('Item 2', $item2);
+
+        $item3 = $this->queue->dequeue();
+
+        $this->assertCount(0, $this->queue);
+        $this->assertTrue($this->queue->isEmpty());
+        $this->assertSame('Item 3', $item3);
+    }
+
+    public function testQueuePeek(): void
+    {
+        $this->queue->enqueue('Item 1');
+        $this->queue->enqueue('Item 2');
+        $this->queue->enqueue('Item 3');
+
+        $this->assertSame('Item 1', $this->queue->peek());
+
+        $this->queue->dequeue();
+
+        $this->assertSame('Item 2', $this->queue->peek());
+
+        $this->queue->dequeue();
+
+        $this->assertSame('Item 3', $this->queue->peek());
+    }
+
+    public function testOverflowExceptionOnQueueLimit(): void
+    {
+        $this->expectException(OverflowException::class);
+
+        $this->queue->enqueue('Item 1');
+        $this->queue->enqueue('Item 2');
+        $this->queue->enqueue('Item 3');
+        $this->queue->enqueue('Item 4');
+        $this->queue->enqueue('Item 5');
+        $this->queue->enqueue('Item 6');
+    }
+
+    public function testUnderflowExceptionOnPopWithEmtpyQueue(): void
+    {
+        $this->expectException(UnderflowException::class);
+        $this->queue->dequeue();
+    }
+}
