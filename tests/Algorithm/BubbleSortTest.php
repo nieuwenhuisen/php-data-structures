01c7edb694365dad779f2cf1f97ffe8e9f9b066d
nieuwenhuisen <nieuwenhuisen@me.com>
2019-09-03 10:16:41 +0200

    Add BubbleSort algorithm

---
+++ tests/Algorithm/BubbleSortTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Tests\Algorithm;
+
+use App\Algorithm\BubbleSort;
+use PHPUnit\Framework\TestCase;
+
+class BubbleSortTest extends TestCase
+{
+    public function testBubbleSort(): void
+    {
+        $input = [3, 7, 6, 9, 4, 1, 8, 2, 5, 10];
+        $expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+        // Iterations two nested loops: 90
+        // Iterations reduced length with ($length - $i - 1): 45
+        // Iterations swapped boolean check: 42
+        // Iterations bound (save last swap position and use as new bound): 34
+        $sorted = BubbleSort::sort($input);
+
+        $this->assertEquals($expected, $sorted);
+    }
+}
