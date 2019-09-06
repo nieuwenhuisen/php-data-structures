4751c7900766479d7ad8ec399d7364d2a5106a0a
nieuwenhuisen <nieuwenhuisen@me.com>
2019-09-06 12:27:57 +0200

    Add quick sort

--- src/Algorithms/QuickSort.php
+++ src/Algorithms/QuickSort.php
@@ -4,8 +4,42 @@ namespace App\Algorithms;
 
 class QuickSort
 {
-    public static function sort(array $input): array
+    public static function sort(array &$input, $p = 0, $r = false): void
     {
-        return $input;
+        if ($r === false) {
+            $r = count($input)-1;
+        }
+
+        if ($p < $r) {
+            $q = self::partition($input, $p, $r);
+            self::sort($input, $p, $q);
+            self::sort($input, $q+1, $r);
+        }
+    }
+
+    public static function partition(array &$input, int $p, int $r): ?int
+    {
+        $pivot = $input[$p];
+        $i = $p-1;
+        $j = $r+1;
+
+        while (true)
+        {
+            do {
+                $i++;
+            } while($input[$i] < $pivot && $input[$i] !== $pivot);
+
+            do {
+                $j--;
+            } while($input[$j] > $pivot && $input[$j] !== $pivot);
+
+            if($i < $j) {
+                $temp = $input[$i];
+                $input[$i] = $input[$j];
+                $input[$j] = $temp;
+            } else {
+                return $j;
+            }
+        }
     }
 }
--- tests/Algorithms/QuickSortTest.php
+++ tests/Algorithms/QuickSortTest.php
@@ -12,8 +12,7 @@ class QuickSortTest extends TestCase
         $input = [3, 7, 6, 9, 4, 1, 8, 2, 5, 10];
         $expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-        $sorted = QuickSort::sort($input);
-
-        //$this->assertEquals($expected, $sorted);
+        QuickSort::sort($input);
+        $this->assertEquals($expected, $input);
     }
 }
