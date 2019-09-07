6a52639bf2fe4a85f7295b0b3cf9d5b878c547b5
Jeroen van den Nieuwenhuisen <nieuwenhuisen@me.com>
2019-09-07 21:05:11 +0200

    Add bucket sort

--- src/Algorithms/BucketSort.php
+++ src/Algorithms/BucketSort.php
@@ -4,8 +4,35 @@ namespace App\Algorithms;
 
 class BucketSort
 {
-    public static function sort(array $input): array
+    public static function sort(array &$input)
     {
+        $inputCount = count($input);
+
+        if ($inputCount <= 0) {
+            return;
+        }
+
+        $min = min($input);
+        $max = max($input);
+        $bucketLength = $max - $min + 1;
+
+        $bucket = array_fill(0, $bucketLength, []);
+
+        for ($i = 0; $i < $inputCount; $i++) {
+            $bucket[$input[$i] - $min][] = $input[$i];
+        }
+
+        $k = 0;
+
+        for ($i = 0; $i < $bucketLength; $i++) {
+            $bucketCount = count($bucket[$i]);
+
+            for ($j = 0; $j < $bucketCount; $j++) {
+                $input[$k] = $bucket[$i][$j];
+                $k++;
+            }
+        }
+
         return $input;
     }
 }
