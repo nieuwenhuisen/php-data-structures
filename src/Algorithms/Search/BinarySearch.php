d6fc18914f63a28f627dd6268f63e38d1f041594
nieuwenhuisen <nieuwenhuisen@me.com>
2019-09-09 10:13:55 +0200

    Add binary search

--- src/Algorithms/Search/BinarySearch.php
+++ src/Algorithms/Search/BinarySearch.php
@@ -4,4 +4,23 @@ namespace App\Algorithms\Search;
 
 class BinarySearch
 {
+    public static function search(array $input, $needle): bool
+    {
+        $low = 0;
+        $high = count($input) - 1;
+
+        while ($low <= $high) {
+            $mid = (int)(($low + $high) / 2);
+
+            if ($input[$mid] > $needle) {
+                $high = $mid - 1;
+            } else if ($input[$mid] < $needle) {
+                $low = $mid + 1;
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
---
+++ tests/Algorithms/Search/BinarySearchTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Tests\Algorithms\Search;
+
+use App\Algorithms\Search\BinarySearch;
+use PHPUnit\Framework\TestCase;
+
+class BinarySearchTest extends TestCase
+{
+    public function testSearch(): void
+    {
+        // Binary Search only works with sorted lists
+        $input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 12];
+        $result1 = BinarySearch::search($input, 6);
+        $result2 = BinarySearch::search($input, 11);
+
+        $this->assertTrue($result1);
+        $this->assertFalse($result2);
+    }
+}
