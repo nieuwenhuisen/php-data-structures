a9432a72c0beeebc6163a902a13cd33fc376a848
nieuwenhuisen <nieuwenhuisen@me.com>
2019-09-25 09:07:13 +0200

    Add node class

---
+++ src/DataStructures/Tree/Node.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\DataStructures\Tree;
+
+class Node
+{
+    public $data;
+    public $left;
+    public $right;
+
+    public function __construct(int $data = null)
+    {
+        $this->data = $data;
+    }
+
+    public function min(): Node
+    {
+        $node = $this;
+
+        while ($node->left) {
+            $node = $node->left;
+        }
+
+        return $node;
+    }
+
+    public function max(): Node
+    {
+        $node = $this;
+
+        while ($node->right) {
+            $node = $node->right;
+        }
+
+        return $node;
+    }
+
+    public function successor(): ?Node
+    {
+        $node = $this;
+
+        if ($node->right) {
+            /** @var Node $right */
+            $right = $node->right;
+            return $right->min();
+        }
+
+        return null;
+    }
+
+    public function predecessor(): ?Node
+    {
+        $node = $this;
+
+        if ($node->left) {
+            /** @var Node $left */
+            $left = $node->left;
+            return $left->max();
+        }
+
+        return null;
+    }
+}
