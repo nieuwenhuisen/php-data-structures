0e7d80b2841d65a3a2fda969d9758eb90d650b7a
nieuwenhuisen <nieuwenhuisen@me.com>
2020-01-08 13:55:20 +0100

    Remove static cache array

--- src/Algorithms/Recursion/FibonacciMemoized.php
+++ src/Algorithms/Recursion/FibonacciMemoized.php
@@ -4,29 +4,28 @@ namespace App\Algorithms\Recursion;
 
 class FibonacciMemoized
 {
-    private static $cache = [];
     public static $count = 0;
 
-    public static function fibonacciMemoized(int $number): int
+    public static function fibonacciMemoized(int $number, &$cache = []): int
     {
-        ++self::$count;
+        ++static::$count;
 
         if (0 === $number || 1 === $number) {
             return 1;
         }
 
-        if (isset(self::$cache[$number - 1])) {
-            $tmp1 = self::$cache[$number - 1];
+        if (isset($cache[$number - 1])) {
+            $tmp1 = $cache[$number - 1];
         } else {
-            $tmp1 = self::fibonacciMemoized($number - 1);
-            self::$cache[$number - 1] = $tmp1;
+            $tmp1 = static::fibonacciMemoized($number - 1, $cache);
+            $cache[$number - 1] = $tmp1;
         }
 
-        if (isset(self::$cache[$number - 2])) {
-            $tmp2 = self::$cache[$number - 2];
+        if (isset($cache[$number - 2])) {
+            $tmp2 = $cache[$number - 2];
         } else {
-            $tmp2 = self::fibonacciMemoized($number - 2);
-            self::$cache[$number - 2] = $tmp2;
+            $tmp2 = static::fibonacciMemoized($number - 2, $cache);
+            $cache[$number - 2] = $tmp2;
         }
 
         return $tmp1 + $tmp2;
